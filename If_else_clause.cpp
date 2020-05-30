0c8efa0447f3b8ffe2d7b5f3d5cd8cb794016030
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 17:08:33 -0500

    Declaring a variable for password, another one for input and the output for the program

---
+++ If_else_clause.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	
+	string password="Kiribati";
+	string input;
+
+	cout<<"Enter your password: "<<flush;
+	cin>>input;
+	
+
+	cout<<"1. Saving new information"<<endl;
+	cout<<"2. Deleting new information"<<endl;
+	cout<<"3. Quit"<<endl;
+
+
+
+	return 0;
+}
