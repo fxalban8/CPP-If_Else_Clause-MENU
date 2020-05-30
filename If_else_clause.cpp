8737e1e8d365f883a33e391de747512eddaf0737
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 17:12:28 -0500

    Verifying authentication

--- If_else_clause.cpp
+++ If_else_clause.cpp
@@ -10,12 +10,14 @@ int main(){
 	cout<<"Enter your password: "<<flush;
 	cin>>input;
 	
-
-	cout<<"1. Saving new information"<<endl;
-	cout<<"2. Deleting new information"<<endl;
-	cout<<"3. Quit"<<endl;
-
-
+	if(input==password){
+		cout<<"Welcome, user. What do you want to do?"<<endl;
+		cout<<"1. Saving new information"<<endl;
+		cout<<"2. Deleting new information"<<endl;
+		cout<<"3. Quit"<<endl;
+	}else{
+		cout<<"Access denied"<<endl;
+	}
 
 	return 0;
 }
