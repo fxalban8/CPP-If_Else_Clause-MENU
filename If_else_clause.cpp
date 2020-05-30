8c019f833e9f0cb2a58ba634e8c1174e7b0a8f5d
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 17:35:01 -0500

    Deploy a menu of options, function sleep from unistd is incorporated

--- If_else_clause.cpp
+++ If_else_clause.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>	//library for the function: sleep
 using namespace std;
 
 
@@ -15,6 +16,25 @@ int main(){
 		cout<<"1. Saving new information"<<endl;
 		cout<<"2. Deleting new information"<<endl;
 		cout<<"3. Quit"<<endl;
+	
+	
+		int option=0;// in order not to collect garbage, x = 0
+		cin>>option;
+		cout<<"Processing information"<<endl;
+		sleep(5);	//wait for 5 secons util you go for the next instruction
+
+		if(option==1){
+			cout<<"Information saved"<<endl;
+		}else{
+			if(option==2){
+				cout<<"Information deleted"<<endl;
+			}else{
+				cout<<"quitting...";
+			}
+
+		}
+		
+	
 	}else{
 		cout<<"Access denied"<<endl;
 	}
